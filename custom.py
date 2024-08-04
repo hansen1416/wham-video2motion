4436e2912b7dc23bfb51783e8b04047b2eff3ec9
hansen1416 <badapplesweetie@gmail.com>
2024-08-04 16:49:05 +0800

    1

--- custom.py
+++ custom.py
@@ -15,6 +15,9 @@ from lib.models import build_network, build_body_model
 from lib.models.preproc.detector import DetectionModel
 from lib.models.preproc.extractor import FeatureExtractor
 from lib.data.datasets import CustomDataset
+from lib.utils.imutils import avg_preds
+from lib.models.smplify import TemporalSMPLify
+from lib.utils.transforms import matrix_to_axis_angle
 
 parser = argparse.ArgumentParser()
 
@@ -138,3 +141,152 @@ with torch.no_grad():
 
     # run WHAM
     results = defaultdict(dict)
+
+    print(f"lenth of dataset: {len(dataset)}")
+
+    n_subjs = len(dataset)
+    for subj in range(n_subjs):
+
+        with torch.no_grad():
+            if cfg.FLIP_EVAL:
+                # Forward pass with flipped input
+                flipped_batch = dataset.load_data(subj, True)
+                (
+                    _id,
+                    x,
+                    inits,
+                    features,
+                    mask,
+                    init_root,
+                    cam_angvel,
+                    frame_id,
+                    kwargs,
+                ) = flipped_batch
+                flipped_pred = network(
+                    x,
+                    inits,
+                    features,
+                    mask=mask,
+                    init_root=init_root,
+                    cam_angvel=cam_angvel,
+                    return_y_up=True,
+                    **kwargs,
+                )
+
+                # Forward pass with normal input
+                batch = dataset.load_data(subj)
+                (
+                    _id,
+                    x,
+                    inits,
+                    features,
+                    mask,
+                    init_root,
+                    cam_angvel,
+                    frame_id,
+                    kwargs,
+                ) = batch
+                pred = network(
+                    x,
+                    inits,
+                    features,
+                    mask=mask,
+                    init_root=init_root,
+                    cam_angvel=cam_angvel,
+                    return_y_up=True,
+                    **kwargs,
+                )
+
+                # Merge two predictions
+                flipped_pose, flipped_shape = flipped_pred["pose"].squeeze(
+                    0
+                ), flipped_pred["betas"].squeeze(0)
+                pose, shape = pred["pose"].squeeze(0), pred["betas"].squeeze(0)
+                flipped_pose, pose = flipped_pose.reshape(-1, 24, 6), pose.reshape(
+                    -1, 24, 6
+                )
+                avg_pose, avg_shape = avg_preds(
+                    pose, shape, flipped_pose, flipped_shape
+                )
+                avg_pose = avg_pose.reshape(-1, 144)
+                avg_contact = (
+                    flipped_pred["contact"][..., [2, 3, 0, 1]] + pred["contact"]
+                ) / 2
+
+                # Refine trajectory with merged prediction
+                network.pred_pose = avg_pose.view_as(network.pred_pose)
+                network.pred_shape = avg_shape.view_as(network.pred_shape)
+                network.pred_contact = avg_contact.view_as(network.pred_contact)
+                output = network.forward_smpl(**kwargs)
+                pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
+
+            else:
+                # data
+                batch = dataset.load_data(subj)
+                (
+                    _id,
+                    x,
+                    inits,
+                    features,
+                    mask,
+                    init_root,
+                    cam_angvel,
+                    frame_id,
+                    kwargs,
+                ) = batch
+
+                # inference
+                pred = network(
+                    x,
+                    inits,
+                    features,
+                    mask=mask,
+                    init_root=init_root,
+                    cam_angvel=cam_angvel,
+                    return_y_up=True,
+                    **kwargs,
+                )
+
+        print(f"pred: {pred}")
+
+        # if False:
+        if args.run_smplify:
+            smplify = TemporalSMPLify(
+                smpl, img_w=width, img_h=height, device=cfg.DEVICE
+            )
+            input_keypoints = dataset.tracking_results[_id]["keypoints"]
+            pred = smplify.fit(pred, input_keypoints, **kwargs)
+
+            with torch.no_grad():
+                network.pred_pose = pred["pose"]
+                network.pred_shape = pred["betas"]
+                network.pred_cam = pred["cam"]
+                output = network.forward_smpl(**kwargs)
+                pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
+
+        # ========= Store results ========= #
+        pred_body_pose = (
+            matrix_to_axis_angle(pred["poses_body"]).cpu().numpy().reshape(-1, 69)
+        )
+        pred_root = (
+            matrix_to_axis_angle(pred["poses_root_cam"]).cpu().numpy().reshape(-1, 3)
+        )
+        pred_root_world = (
+            matrix_to_axis_angle(pred["poses_root_world"]).cpu().numpy().reshape(-1, 3)
+        )
+        pred_pose = np.concatenate((pred_root, pred_body_pose), axis=-1)
+        pred_pose_world = np.concatenate((pred_root_world, pred_body_pose), axis=-1)
+        pred_trans = (pred["trans_cam"] - network.output.offset).cpu().numpy()
+
+        results[_id]["pose"] = pred_pose
+        results[_id]["trans"] = pred_trans
+        results[_id]["pose_world"] = pred_pose_world
+        results[_id]["trans_world"] = pred["trans_world"].cpu().squeeze(0).numpy()
+        results[_id]["betas"] = pred["betas"].cpu().squeeze(0).numpy()
+        results[_id]["verts"] = (
+            (pred["verts_cam"] + pred["trans_cam"].unsqueeze(1)).cpu().numpy()
+        )
+        results[_id]["frame_ids"] = frame_id
+
+    if True:
+        joblib.dump(results, os.path.join(output_pth, "wham_output.pkl"))
