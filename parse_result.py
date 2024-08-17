2166d5202bcd956fc1c1a6dd8072b0c84fb2f4ae
hansen1416 <badapplesweetie@gmail.com>
2024-08-17 12:19:25 +0800

    1

---
+++ parse_result.py
@@ -0,0 +1,26 @@
+import joblib
+import os
+
+import numpy as np
+
+output_pth = os.path.join(".", "output")
+
+# Assuming output_pth is defined
+output_file = os.path.join(output_pth, "madfit1", "wham_output.pkl")
+loaded_results = joblib.load(output_file)
+
+pose = loaded_results[0]["pose"]
+trans = loaded_results[0]["trans"]
+pose_world = loaded_results[0]["pose_world"]
+trans_world = loaded_results[0]["trans_world"]
+betas = loaded_results[0]["betas"]
+verts = loaded_results[0]["verts"]
+frame_ids = loaded_results[0]["frame_ids"]
+
+
+for pose_frame in pose:
+    # print(pose.shape)
+
+    axis_angles = pose_frame.reshape((24, 3))
+
+    print(axis_angles.shape)
