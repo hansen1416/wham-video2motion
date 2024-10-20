1841e167f5d1ddb0a913e91c0fb308c80a5dd1ea
hansen1416 <badapplesweetie@gmail.com>
2024-10-20 22:37:57 +0800

    1

--- custom.py
+++ custom.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from glob import glob
 from collections import defaultdict
+from typing import Tuple
 
 import cv2
 import torch
@@ -18,6 +19,14 @@ from lib.data.datasets import CustomDataset
 from lib.utils.imutils import avg_preds
 from lib.models.smplify import TemporalSMPLify
 from lib.utils.transforms import matrix_to_axis_angle
+
+video_name = "madfit3"
+video_path = os.path.join("videos", f"{video_name}.mp4")
+
+output_pth = os.path.join(".", "output", video_name)
+
+if not os.path.exists(output_pth):
+    os.makedirs(output_pth)
 
 parser = argparse.ArgumentParser()
 
@@ -70,27 +79,30 @@ smpl = build_body_model(cfg.DEVICE, smpl_batch_size)
 network = build_network(cfg, smpl)
 network.eval()
 
-# print(network)
 
-video_name = "madfit3"
-
-video = os.path.join("videos", f"{video_name}.mp4")
-
-cap = cv2.VideoCapture(video)
-assert cap.isOpened(), f"Faild to load video file {video}"
+cap = cv2.VideoCapture(video_path)
+assert cap.isOpened(), f"Faild to load video file {video_path}"
 
 fps = cap.get(cv2.CAP_PROP_FPS)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 width, height = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-output_pth = os.path.join(".", "output", video_name)
 
-with torch.no_grad():
+def get_tracks_slam_results(
+    video_path, output_pth, cfg
+) -> Tuple[defaultdict, np.ndarray]:
 
-    if not (
-        os.path.exists(os.path.join(output_pth, "tracking_results.pth"))
-        and os.path.exists(os.path.join(output_pth, "slam_results.pth"))
-    ):
+    if os.path.exists(
+        os.path.join(output_pth, "tracking_results.pth")
+    ) and os.path.exists(os.path.join(output_pth, "slam_results.pth")):
+
+        tracking_results = joblib.load(os.path.join(output_pth, "tracking_results.pth"))
+        slam_results = joblib.load(os.path.join(output_pth, "slam_results.pth"))
+        print(f"Already processed data exists at {output_pth} ! Load the data .")
+
+        return tracking_results, slam_results
+
+    with torch.no_grad():
 
         # `cfg.DEVICE.lower()` cuda
         detector = DetectionModel(cfg.DEVICE.lower())
@@ -125,170 +137,165 @@ with torch.no_grad():
 
         # Extract image features
         # TODO: Merge this into the previous while loop with an online bbox smoothing.
-        tracking_results = extractor.run(video, tracking_results)
+        tracking_results = extractor.run(video_path, tracking_results)
 
         print(tracking_results)
 
         joblib.dump(tracking_results, os.path.join(output_pth, "tracking_results.pth"))
         joblib.dump(slam_results, os.path.join(output_pth, "slam_results.pth"))
 
-    else:
+        return tracking_results, slam_results
 
-        tracking_results = joblib.load(os.path.join(output_pth, "tracking_results.pth"))
-        slam_results = joblib.load(os.path.join(output_pth, "slam_results.pth"))
-        print(f"Already processed data exists at {output_pth} ! Load the data .")
 
-    # Build dataset
-    dataset = CustomDataset(cfg, tracking_results, slam_results, width, height, fps)
+tracking_results, slam_results = get_tracks_slam_results(video_path, output_pth, cfg)
 
-    # run WHAM
-    results = defaultdict(dict)
+# Build dataset
+dataset = CustomDataset(cfg, tracking_results, slam_results, width, height, fps)
 
-    print(f"lenth of dataset: {len(dataset)}")
+# run WHAM
+results = defaultdict(dict)
 
-    n_subjs = len(dataset)
-    for subj in range(n_subjs):
+print(f"lenth of dataset: {len(dataset)}")
+
+n_subjs = len(dataset)
+for subj in range(n_subjs):
+
+    with torch.no_grad():
+        if cfg.FLIP_EVAL:
+            # Forward pass with flipped input
+            flipped_batch = dataset.load_data(subj, True)
+            (
+                _id,
+                x,
+                inits,
+                features,
+                mask,
+                init_root,
+                cam_angvel,
+                frame_id,
+                kwargs,
+            ) = flipped_batch
+            flipped_pred = network(
+                x,
+                inits,
+                features,
+                mask=mask,
+                init_root=init_root,
+                cam_angvel=cam_angvel,
+                return_y_up=True,
+                **kwargs,
+            )
+
+            # Forward pass with normal input
+            batch = dataset.load_data(subj)
+            (
+                _id,
+                x,
+                inits,
+                features,
+                mask,
+                init_root,
+                cam_angvel,
+                frame_id,
+                kwargs,
+            ) = batch
+            pred = network(
+                x,
+                inits,
+                features,
+                mask=mask,
+                init_root=init_root,
+                cam_angvel=cam_angvel,
+                return_y_up=True,
+                **kwargs,
+            )
+
+            # Merge two predictions
+            flipped_pose, flipped_shape = flipped_pred["pose"].squeeze(0), flipped_pred[
+                "betas"
+            ].squeeze(0)
+            pose, shape = pred["pose"].squeeze(0), pred["betas"].squeeze(0)
+            flipped_pose, pose = flipped_pose.reshape(-1, 24, 6), pose.reshape(
+                -1, 24, 6
+            )
+            avg_pose, avg_shape = avg_preds(pose, shape, flipped_pose, flipped_shape)
+            avg_pose = avg_pose.reshape(-1, 144)
+            avg_contact = (
+                flipped_pred["contact"][..., [2, 3, 0, 1]] + pred["contact"]
+            ) / 2
+
+            # Refine trajectory with merged prediction
+            network.pred_pose = avg_pose.view_as(network.pred_pose)
+            network.pred_shape = avg_shape.view_as(network.pred_shape)
+            network.pred_contact = avg_contact.view_as(network.pred_contact)
+            output = network.forward_smpl(**kwargs)
+            pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
+
+        else:
+            # data
+            batch = dataset.load_data(subj)
+            (
+                _id,
+                x,
+                inits,
+                features,
+                mask,
+                init_root,
+                cam_angvel,
+                frame_id,
+                kwargs,
+            ) = batch
+
+            # inference
+            pred = network(
+                x,
+                inits,
+                features,
+                mask=mask,
+                init_root=init_root,
+                cam_angvel=cam_angvel,
+                return_y_up=True,
+                **kwargs,
+            )
+
+    print(f"pred: {pred}")
+
+    # if False:
+    if args.run_smplify:
+        smplify = TemporalSMPLify(smpl, img_w=width, img_h=height, device=cfg.DEVICE)
+        input_keypoints = dataset.tracking_results[_id]["keypoints"]
+        pred = smplify.fit(pred, input_keypoints, **kwargs)
 
         with torch.no_grad():
-            if cfg.FLIP_EVAL:
-                # Forward pass with flipped input
-                flipped_batch = dataset.load_data(subj, True)
-                (
-                    _id,
-                    x,
-                    inits,
-                    features,
-                    mask,
-                    init_root,
-                    cam_angvel,
-                    frame_id,
-                    kwargs,
-                ) = flipped_batch
-                flipped_pred = network(
-                    x,
-                    inits,
-                    features,
-                    mask=mask,
-                    init_root=init_root,
-                    cam_angvel=cam_angvel,
-                    return_y_up=True,
-                    **kwargs,
-                )
+            network.pred_pose = pred["pose"]
+            network.pred_shape = pred["betas"]
+            network.pred_cam = pred["cam"]
+            output = network.forward_smpl(**kwargs)
+            pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
 
-                # Forward pass with normal input
-                batch = dataset.load_data(subj)
-                (
-                    _id,
-                    x,
-                    inits,
-                    features,
-                    mask,
-                    init_root,
-                    cam_angvel,
-                    frame_id,
-                    kwargs,
-                ) = batch
-                pred = network(
-                    x,
-                    inits,
-                    features,
-                    mask=mask,
-                    init_root=init_root,
-                    cam_angvel=cam_angvel,
-                    return_y_up=True,
-                    **kwargs,
-                )
+    # ========= Store results ========= #
+    pred_body_pose = (
+        matrix_to_axis_angle(pred["poses_body"]).cpu().numpy().reshape(-1, 69)
+    )
+    pred_root = (
+        matrix_to_axis_angle(pred["poses_root_cam"]).cpu().numpy().reshape(-1, 3)
+    )
+    pred_root_world = (
+        matrix_to_axis_angle(pred["poses_root_world"]).cpu().numpy().reshape(-1, 3)
+    )
+    pred_pose = np.concatenate((pred_root, pred_body_pose), axis=-1)
+    pred_pose_world = np.concatenate((pred_root_world, pred_body_pose), axis=-1)
+    pred_trans = (pred["trans_cam"] - network.output.offset).cpu().numpy()
 
-                # Merge two predictions
-                flipped_pose, flipped_shape = flipped_pred["pose"].squeeze(
-                    0
-                ), flipped_pred["betas"].squeeze(0)
-                pose, shape = pred["pose"].squeeze(0), pred["betas"].squeeze(0)
-                flipped_pose, pose = flipped_pose.reshape(-1, 24, 6), pose.reshape(
-                    -1, 24, 6
-                )
-                avg_pose, avg_shape = avg_preds(
-                    pose, shape, flipped_pose, flipped_shape
-                )
-                avg_pose = avg_pose.reshape(-1, 144)
-                avg_contact = (
-                    flipped_pred["contact"][..., [2, 3, 0, 1]] + pred["contact"]
-                ) / 2
+    results[_id]["pose"] = pred_pose
+    results[_id]["trans"] = pred_trans
+    results[_id]["pose_world"] = pred_pose_world
+    results[_id]["trans_world"] = pred["trans_world"].cpu().squeeze(0).numpy()
+    results[_id]["betas"] = pred["betas"].cpu().squeeze(0).numpy()
+    results[_id]["verts"] = (
+        (pred["verts_cam"] + pred["trans_cam"].unsqueeze(1)).cpu().numpy()
+    )
+    results[_id]["frame_ids"] = frame_id
 
-                # Refine trajectory with merged prediction
-                network.pred_pose = avg_pose.view_as(network.pred_pose)
-                network.pred_shape = avg_shape.view_as(network.pred_shape)
-                network.pred_contact = avg_contact.view_as(network.pred_contact)
-                output = network.forward_smpl(**kwargs)
-                pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
 
-            else:
-                # data
-                batch = dataset.load_data(subj)
-                (
-                    _id,
-                    x,
-                    inits,
-                    features,
-                    mask,
-                    init_root,
-                    cam_angvel,
-                    frame_id,
-                    kwargs,
-                ) = batch
-
-                # inference
-                pred = network(
-                    x,
-                    inits,
-                    features,
-                    mask=mask,
-                    init_root=init_root,
-                    cam_angvel=cam_angvel,
-                    return_y_up=True,
-                    **kwargs,
-                )
-
-        print(f"pred: {pred}")
-
-        # if False:
-        if args.run_smplify:
-            smplify = TemporalSMPLify(
-                smpl, img_w=width, img_h=height, device=cfg.DEVICE
-            )
-            input_keypoints = dataset.tracking_results[_id]["keypoints"]
-            pred = smplify.fit(pred, input_keypoints, **kwargs)
-
-            with torch.no_grad():
-                network.pred_pose = pred["pose"]
-                network.pred_shape = pred["betas"]
-                network.pred_cam = pred["cam"]
-                output = network.forward_smpl(**kwargs)
-                pred = network.refine_trajectory(output, cam_angvel, return_y_up=True)
-
-        # ========= Store results ========= #
-        pred_body_pose = (
-            matrix_to_axis_angle(pred["poses_body"]).cpu().numpy().reshape(-1, 69)
-        )
-        pred_root = (
-            matrix_to_axis_angle(pred["poses_root_cam"]).cpu().numpy().reshape(-1, 3)
-        )
-        pred_root_world = (
-            matrix_to_axis_angle(pred["poses_root_world"]).cpu().numpy().reshape(-1, 3)
-        )
-        pred_pose = np.concatenate((pred_root, pred_body_pose), axis=-1)
-        pred_pose_world = np.concatenate((pred_root_world, pred_body_pose), axis=-1)
-        pred_trans = (pred["trans_cam"] - network.output.offset).cpu().numpy()
-
-        results[_id]["pose"] = pred_pose
-        results[_id]["trans"] = pred_trans
-        results[_id]["pose_world"] = pred_pose_world
-        results[_id]["trans_world"] = pred["trans_world"].cpu().squeeze(0).numpy()
-        results[_id]["betas"] = pred["betas"].cpu().squeeze(0).numpy()
-        results[_id]["verts"] = (
-            (pred["verts_cam"] + pred["trans_cam"].unsqueeze(1)).cpu().numpy()
-        )
-        results[_id]["frame_ids"] = frame_id
-
-    if True:
-        joblib.dump(results, os.path.join(output_pth, "wham_output.pkl"))
+joblib.dump(results, os.path.join(output_pth, "wham_output.pkl"))
