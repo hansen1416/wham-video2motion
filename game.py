27ac92677d273b45447ea50b47af8966ceff9ca7
hansen1416 <badapplesweetie@gmail.com>
2024-10-28 21:11:57 +0800

    1

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ class MeshPlayer:
     WIDTH, HEIGHT = 800, 600
     FPS = 60
 
-    def __init__(self, caption=None, vertices=None, faces=None):
+    def __init__(self, caption=None, vertices_frame=None, faces=None):
 
         # Initialize Pygame
         pygame.init()
@@ -20,8 +20,11 @@ class MeshPlayer:
         if caption:
             pygame.display.set_caption(caption)
 
-        self.vertices = vertices
+        self.vertices_frame = vertices_frame
         self.faces = faces
+
+        self.vertices = self.vertices_frame[0]
+        self.frame = 0
 
         self.angle_y = 0  # Initial rotation angle around Y axis
 
@@ -42,15 +45,6 @@ class MeshPlayer:
         )
 
         self.vertices = np.dot(self.vertices, rotation_matrix)
-
-        self.angle_y = 0
-
-    def _update(self):
-        """Randomly update the vertices of the cube."""
-        for i in range(len(self.vertices)):
-            # Randomly adjust each vertex position slightly
-            self.vertices[i] += np.random.normal(0, 0.01, size=3)  # Small random change
-        # return vertices
 
     # Projection parameters
     def _project(self, vertex):
@@ -73,18 +67,24 @@ class MeshPlayer:
                     # Clear the screen
             self.screen.fill((0, 0, 0))  # Black background
 
-            self._update()
-
             self._control()
 
             # Draw the faces of the cube
-            for face in faces:
+            for face in self.faces:
                 polygon = [self._project(self.vertices[i]) for i in face]
                 pygame.draw.polygon(
                     self.screen, (255, 255, 255), polygon, width=0
                 )  # Draw the face outline
 
             pygame.display.flip()
+
+            self.frame += 1
+
+            if self.frame >= len(self.vertices_frame):
+                self.frame = 0
+
+            self.vertices = self.vertices_frame[self.frame]
+
             clock.tick(MeshPlayer.FPS)
 
 
@@ -106,6 +106,22 @@ if __name__ == "__main__":
         dtype=np.float64,
     )
 
+    def update_vertices(base_vertices):
+
+        frame_vertices = []
+
+        """Randomly update the vertices of the cube."""
+        for i in range(100):
+            tmp_vertices = np.copy(base_vertices)
+            # Randomly adjust each vertex position slightly
+            tmp_vertices += np.random.normal(0, 0.01, size=3)  # Small random change
+
+            frame_vertices.append(tmp_vertices)
+
+        return np.array(frame_vertices)
+
+    all_vertices = update_vertices(vertices)
+
     # Define the faces of the cube (each face is defined by a list of vertex indices)
     faces = [
         [0, 1, 2],  # Back face
@@ -116,6 +132,6 @@ if __name__ == "__main__":
         [1, 2, 6],  # Right face
     ]
 
-    mp = MeshPlayer(vertices=vertices, faces=faces)
+    mp = MeshPlayer(vertices_frame=all_vertices, faces=faces)
 
     mp.run()
